ba27f4f1311c42107b41a176d5901463d05ff266
Acceleration17 <acceleration17@gmail.com>
2022-09-29 13:50:46 -0400

    Add files via upload
    Input of noised sin wave, output of preisach model and hysteresis graph.

---
+++ Preisachmodel.m
@@ -0,0 +1,128 @@
+
+global datainput;
+
+t = 0:0.01:5;  % time
+n_t = length(t);
+fs = 0.5;  % frequency of signal
+signal = (sin(2*pi.*fs.*t)-1)*0.8+0.8;
+fn = 10; % frequency of noise
+noise_Amplitude = 0.2;
+sine_noise = noise_Amplitude.*sin(2*pi.*fn.*t);
+
+figure1=figure(1);
+
+n=100;
+a0=1;
+regular=1;
+mu=generateMu(n,a0,regular);
+
+datainput=signal+sine_noise;
+ftable=[];
+
+[ff,muf]=DiscretePreisach(ncount,datainput,mu,n);
+ ftable=[ftable ff];
+
+
+
+
+for ncount=2:501
+   
+    subplot(1,3,1);
+   
+   hold on
+   plot(t(1:ncount),datainput(1:ncount),'r') ;
+   axis([0 5 -2 2]);
+   pause(0.05);
+   
+   
+   
+   %%Start of plot 2%%
+   
+   subplot(1,3,2);
+    plot(mu(2,:),mu(1,:),'squarer','MarkerSize',14,'MarkerFaceColor',[1 0 0])
+    axis([-a0 a0 -a0 a0]);
+    hold on
+
+    
+   uNew=datainput(ncount);
+    
+    [f,mu]=DiscretePreisach(ncount,datainput,mu,n);
+    ftable=[ftable f];
+
+    
+    countOn=0;
+    countOff=0;
+    N=n*(n+1)/2;
+    
+    for i=1:N
+        if mu(3,i)==1
+            countOn=countOn+1;
+            muOn(1,countOn)=mu(1,i);
+            muOn(2,countOn)=mu(2,i);
+        else
+            countOff=countOff+1;
+            muOff(1,countOff)=mu(1,i);
+            muOff(2,countOff)=mu(2,i);
+        end
+    end
+    
+
+    subplot(1,3,2);
+    if exist('muOn','var') %if any hysterons are 'up' or 'on,' plot them
+        plot(muOn(2,:),muOn(1,:),'squareb','MarkerSize',14,'MarkerFaceColor',[0 0 1])
+        axis([-a0 a0 -a0 a0]);
+        pause(0.05)
+    end
+     
+    
+    if exist('muOff','var') %if any hysterons are 'down' or 'off,' plot them
+        plot(muOff(2,:),muOff(1,:),'squarer','MarkerSize',14,'MarkerFaceColor',[1 0 0])
+        axis([-a0 a0 -a0 a0]);
+        pause(0.05)
+    end
+    
+
+    clearvars muOn muOff
+    
+    
+    
+    
+    
+    %%Start of Plot 3%%
+    N=n*(n+1)/2;
+    subplot(1,3,3);
+
+    
+    
+     h = plot(NaN,NaN); 
+     
+      hLine = line('XData',datainput(1), 'YData',ftable(1), 'Color','r', ...
+    'Marker','o', 'MarkerSize',6, 'LineWidth',2);
+hTxt = text(datainput(1), ftable(1), sprintf('(%.3f,%.3f)',datainput(1),ftable(1)), ...
+    'Color',[0.2 0.2 0.2], 'FontSize',8, ...
+    'HorizontalAlignment','left', 'VerticalAlignment','top');
+
+     axis([-a0 a0 -N N]); 
+     
+        pause(0.01)
+        set(h, 'XData', datainput(2:ncount), 'YData', ftable(2:ncount));
+         
+        
+     set(hLine, 'XData',datainput(ncount), 'YData',ftable(ncount))   
+    set(hTxt, 'Position',[datainput(ncount) ftable(ncount)], ...
+        'String',sprintf('(%.3f,%.3f)',[datainput(ncount) ftable(ncount)]))        
+    drawnow 
+     
+     
+%     xlabel('Input','FontSize',14)
+%     ylabel('Output','FontSize',14)
+%     xlabh = get(gca,'XLabel');
+%     set(xlabh,'Position',get(xlabh,'Position') + [0 .01 0])
+%     xlabh = get(gca,'YLabel');
+%     set(xlabh,'Position',get(xlabh,'Position') + [.05 0 0])
+
+end
+
+
+
+
